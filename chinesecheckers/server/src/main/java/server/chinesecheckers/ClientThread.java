656d78f37b16d8eb1f6e1bb999abc191c59ab7a6
Błażej Pawluk <bpawluk05@gmail.com>
2024-12-06 15:41:58 +0100

    clientThread v2 (number, changeNumber())

--- chinesecheckers/server/src/main/java/server/chinesecheckers/ClientThread.java
+++ chinesecheckers/server/src/main/java/server/chinesecheckers/ClientThread.java
@@ -4,8 +4,12 @@ import java.net.*;
 
 public class ClientThread extends Thread {
 
-    public ClientThread(Socket socket) {
+    public ClientThread(Socket socket, int number) {
         
+    }
+
+    public void changeNumber(int number) {
+
     }
 
 }
