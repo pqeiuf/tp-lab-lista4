432c6098cd4d03fcd70dade76610f29f901ab01d
Błażej Pawluk <bpawluk05@gmail.com>
2024-12-06 15:30:00 +0100

    serwer v1

--- chinesecheckers/server/src/main/java/server/chinesecheckers/ServerApp.java
+++ chinesecheckers/server/src/main/java/server/chinesecheckers/ServerApp.java
@@ -1,13 +1,21 @@
 package server.chinesecheckers;
 
-/**
- * Hello world!
- *
- */
-public class ServerApp 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello! I am server!" );
+import java.net.*;
+
+public class ServerApp {
+    public static void main( String[] args ) {
+        int port = 4444;
+        try {
+            ServerSocket serverSocket = new ServerSocket(port);
+            System.out.println("Server listening on port: " + port);
+
+            while (true) {
+                Socket socket = serverSocket.accept();
+                System.out.println("New player joined.");
+                new ClientThread(socket).start();
+            }
+        } catch (Exception e) {
+            System.err.println("ERROR: " + e);
+        }
     }
 }
