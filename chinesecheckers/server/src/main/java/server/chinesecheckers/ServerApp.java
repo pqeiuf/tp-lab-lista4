65141082b597a2329a07f30b61beb5851fcf4d9b
Błażej Pawluk <bpawluk05@gmail.com>
2024-12-06 15:42:47 +0100

    server v3 (poprawione ustawianie klientow)

--- chinesecheckers/server/src/main/java/server/chinesecheckers/ServerApp.java
+++ chinesecheckers/server/src/main/java/server/chinesecheckers/ServerApp.java
@@ -8,18 +8,17 @@ public class ServerApp {
 
     public static void main( String[] args ) {
         int port = 4444;
-        try {
-            ServerSocket serverSocket = new ServerSocket(port);
+        try (ServerSocket serverSocket = new ServerSocket(port);){
             System.out.println("Server listening on port: " + port);
 
             int clientCount = 0;
+            ClientThread[] players = new ClientThread[MAX_PLAYERS + 1];
 
             while (true) {
                 Socket socket = serverSocket.accept();
                 if (clientCount < MAX_PLAYERS) {
-                    System.out.println("New player joined.");
-                    new ClientThread(socket).start();
-                    clientCount++;
+                    players = updatePlayers(players, socket);
+                    clientCount = updateClientCount(players);
                 }
                 else {
                     //komunikat do klienta o rozlaczeniu z powodu limitu
@@ -30,4 +29,37 @@ public class ServerApp {
             System.err.println("ERROR: " + e);
         }
     }
+
+    public static ClientThread[] updatePlayers (ClientThread[] players, Socket socket) {
+        ClientThread[] result = new ClientThread[MAX_PLAYERS + 1];
+        int index = 1;
+
+        for (int i = 1; i <= MAX_PLAYERS; i++) {
+            if (players[i] != null && players[i].getState() != Thread.State.TERMINATED) {
+                result[index] = players[i];
+                result[index].changeNumber(index);
+                index++;
+            }
+        }
+
+        result[index] = new ClientThread(socket, index);
+        result[index].start();
+
+        System.out.println("New player joined.");
+
+        return result;
+    }
+
+    public static int updateClientCount(ClientThread[] players) {
+        int result = 0;
+        
+        for (int i = 1; i <= MAX_PLAYERS; i++) {
+            if (players[i] != null) {
+                result++;
+            }
+        }
+
+        return result;
+    }
+
 }
