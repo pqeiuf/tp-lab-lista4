bd84b9ab4546ee2820d27ca6e210d935064f3774
Błażej Pawluk <bpawluk05@gmail.com>
2024-12-06 15:34:53 +0100

    server v2 (limit graczy)

--- chinesecheckers/server/src/main/java/server/chinesecheckers/ServerApp.java
+++ chinesecheckers/server/src/main/java/server/chinesecheckers/ServerApp.java
@@ -3,16 +3,28 @@ package server.chinesecheckers;
 import java.net.*;
 
 public class ServerApp {
+
+    public static final int MAX_PLAYERS = 6;
+
     public static void main( String[] args ) {
         int port = 4444;
         try {
             ServerSocket serverSocket = new ServerSocket(port);
             System.out.println("Server listening on port: " + port);
 
+            int clientCount = 0;
+
             while (true) {
                 Socket socket = serverSocket.accept();
-                System.out.println("New player joined.");
-                new ClientThread(socket).start();
+                if (clientCount < MAX_PLAYERS) {
+                    System.out.println("New player joined.");
+                    new ClientThread(socket).start();
+                    clientCount++;
+                }
+                else {
+                    //komunikat do klienta o rozlaczeniu z powodu limitu
+                    socket.close();
+                }
             }
         } catch (Exception e) {
             System.err.println("ERROR: " + e);
