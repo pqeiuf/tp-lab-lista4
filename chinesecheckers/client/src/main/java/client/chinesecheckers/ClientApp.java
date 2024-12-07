e8fa907417f1b91cdba681355b6d340b511f7650
Błażej Pawluk <bpawluk05@gmail.com>
2024-12-07 18:42:59 +0100

    client update (sprawdzanie poprawnosci komendy, status gry, sprawdzenie koniecznosci oczekiwania na odpowiedz serwera)

--- chinesecheckers/client/src/main/java/client/chinesecheckers/ClientApp.java
+++ chinesecheckers/client/src/main/java/client/chinesecheckers/ClientApp.java
@@ -13,12 +13,14 @@ public class ClientApp {
     private String playerNickname;
     private int serverPort;
 
+    private boolean inGame;
+
     /**
      * Punkt wejścia do aplikacji klienta
      * @param args muszą zawierać kolejno playerNickname oraz opcjonalnie serverPort
      */
     public static void main(String[] args) {
-        ClientApp clientApp = new ClientApp(args);
+        new ClientApp(args);
     }
 
     /**
@@ -59,26 +61,34 @@ public class ClientApp {
             // Utworzenie bufora na tekst wpisywany w konsoli
             BufferedReader consoleBufferRead = new BufferedReader(new InputStreamReader(System.in));
 
+            inGame = false;
+            boolean expectingResponse = true;
+
             String serverResponse;
+            String readLine;
             String messageToServer;
             do {
-                // Odebranie wiadomości z serwera i jej wyświetlenie
-                serverResponse = in.readLine();
-                // Jeśli response jest null to znaczy że serwer został zamknięty
-                if (serverResponse == null) {
-                    break;
+                if (expectingResponse) {    
+                    serverResponse = in.readLine();
+                    // Jeśli response jest null to znaczy że serwer został zamknięty
+                    if (serverResponse == null) {
+                        break;
+                    }
+                    System.out.println(serverPort + ":" + playerNickname + " > " + serverResponse);
                 }
-                System.out.println(serverPort + ":" + playerNickname + " > " + serverResponse);
 
-                // Wczytanie sygnału który ma być wysłany do serwera
                 System.out.print(serverPort + ":" + playerNickname + " < ");
-                messageToServer = consoleBufferRead.readLine();
+                readLine = consoleBufferRead.readLine();
 
-                // Wysłanie do wiadomości serwera
-                out.println(messageToServer);
+                messageToServer = formatMessage(readLine);
 
+                if (!messageToServer.equals("")) {
+                    out.println(messageToServer);
+                    expectingResponse = true;
+                } else {
+                    expectingResponse = false;
+                }
             } while (!messageToServer.equals("bye"));
-
             socket.close();
 
         } catch (UnknownHostException severNotFoundException) {
@@ -87,5 +97,99 @@ public class ClientApp {
         } catch (IOException IOError) {
             System.out.println("ERROR: " + IOError);
         }
+    }
+
+    private String formatMessage(String readLine) {
+        readLine = readLine.trim();
+
+        String command;
+        String[] args;
+
+        int firstSpaceIndex = readLine.indexOf(' ');
+        if (firstSpaceIndex == -1) {
+            command = readLine;
+            args = new String[0];
+        } else {
+            command = readLine.substring(0, firstSpaceIndex);
+            args = readLine.substring(firstSpaceIndex + 1).trim().split("[\\s,-]");
+        }
+
+        String formattedMessage;
+        switch (command) {
+            case "start":
+                if (!inGame) {
+                    try {
+                        Integer.parseInt(args[0]);
+                        formattedMessage = command + " " + args[0];
+                    } catch(NumberFormatException e) {
+                        System.out.println("Argument error: argument has to be integer");
+                        formattedMessage = "";
+                    } catch(IndexOutOfBoundsException e) {
+                        System.out.println("Argument error: expected start [arg]");
+                        formattedMessage = "";
+                    }
+                } else {
+                    System.out.println("You are already in game!");
+                    formattedMessage = "";
+                }
+                break;
+                
+            case "move":
+                if (inGame) {
+                    try {
+                        Integer.parseInt(args[0]);
+                        Integer.parseInt(args[1]);
+                        Integer.parseInt(args[2]);
+                        Integer.parseInt(args[3]);
+                        formattedMessage = command + " " + args[0] + "," + args[1] + "-" + args[2] + "," + args[3];
+                    } catch(NumberFormatException e) {
+                        System.out.println("Argument error: argument has to be integer");
+                        formattedMessage = "";
+                    } catch(IndexOutOfBoundsException e) {
+                        System.out.println("Argument error: expected move [arg],[arg]-[arg],[arg]");
+                        formattedMessage = "";
+                    }
+                } else {
+                    System.out.println("You can't perform this action while not in game!");
+                    formattedMessage = "";
+                }
+                break;
+
+            case "skip":
+            case "draw":
+                if (inGame) {
+                    formattedMessage = command;
+                } else {
+                    System.out.println("You can't perform this action while not in game!");
+                    formattedMessage = "";
+                }
+                break;
+
+            case "bye":
+            case "exit":
+            case "quit":
+                formattedMessage = "bye";
+                break;
+
+            case "help":
+                if (inGame) {
+                    System.out.println("move [arg],[arg]-[arg][arg] - perform move");
+                    System.out.println("draw - draw a board");
+                    System.out.println("skip - skip your move");
+                } else {
+                    System.out.println("start [arg] - start new game for chosen number of players");
+                }
+
+            default:
+                System.out.println("Unknown command. Try help for list of available commands");
+                formattedMessage = "";
+                break;
+        }
+
+        return formattedMessage;
+    }
+
+    public void gameStatus(boolean newStatus) {
+        inGame = newStatus;
     }
 }
