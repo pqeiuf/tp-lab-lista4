69ae6bd57e430156131e7dff66989747115bb6b5
Błażej Pawluk <bpawluk05@gmail.com>
2024-12-07 18:49:20 +0100

    server update

--- chinesecheckers/server/src/main/java/server/chinesecheckers/ServerApp.java
+++ chinesecheckers/server/src/main/java/server/chinesecheckers/ServerApp.java
@@ -7,21 +7,24 @@ public class ServerApp {
     public static final int DEFAULT_PORT = 4444;
     public static final int MAX_PLAYERS = 6;
 
+    public static int clientCount;
+    public static ClientThread[] players;
+
     public static void main( String[] args ) {
         int port = DEFAULT_PORT;
 
         try (ServerSocket serverSocket = new ServerSocket(port)) {
-            System.out.println("info: Server listening on port " + port);
+            System.out.println("Info: Server listening on port " + port);
 
-            ClientThread[] players = new ClientThread[MAX_PLAYERS + 1];
-            int clientCount = 0;
+            players = new ClientThread[MAX_PLAYERS + 1];
+            clientCount = 0;
 
             while (true) {
                 Socket socket = serverSocket.accept();
 
                 if (clientCount < MAX_PLAYERS) {
-                    players = updatePlayers(players, socket);
-                    clientCount = updateClientCount(players);
+                    players = updatePlayers(socket, true);
+                    clientCount = updateClientCount();
                 }
                 else {
                     //komunikat do klienta o rozlaczeniu z powodu limitu
@@ -31,10 +34,12 @@ public class ServerApp {
             }
         } catch (Exception e) {
             System.err.println("ERROR: " + e);
+            players = updatePlayers(null, false);
+            clientCount = updateClientCount();
         }
     }
 
-    public static ClientThread[] updatePlayers(ClientThread[] players, Socket socket) {
+    public static ClientThread[] updatePlayers(Socket socket, boolean add) {
         ClientThread[] result = new ClientThread[MAX_PLAYERS + 1];
         int index = 1;
 
@@ -46,15 +51,16 @@ public class ServerApp {
             }
         }
 
-        result[index] = new ClientThread(socket, index);
-        result[index].start();
-
-        System.out.println("info: New player joined with number: " + index);
+        if (add) {
+            result[index] = new ClientThread(socket, index);
+            result[index].start();
+            System.out.println("info: New player joined with number: " + index);
+        }
 
         return result;
     }
 
-    public static int updateClientCount(ClientThread[] players) {
+    public static int updateClientCount() {
         int result = 0;
         
         for (int i = 1; i <= MAX_PLAYERS; i++) {
